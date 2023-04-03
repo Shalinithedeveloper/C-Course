7f8e2319e96058da951737c0f2bc658c73f821e9
SHALINI <drshaverma242@gmail.com>
2023-04-03 22:37:43 +0530

    third commit

--- whileloopv6.cpp
+++ whileloopv6.cpp
@@ -1,22 +1,17 @@
 #include<iostream>
 using namespace std;
 
-
 int main()
 {
-
-
-
     int n;
     cin>>n;
 
-    if(n%2==0){
-        cout<<"Even"<<endl;
+    while (n>0)
+    {
+        cout<<n<<endl;
+        cin>>n;
+    }
     
-    }
-    else{
-        cout<<"Odd"<<endl;
-    }
 
     return 0;
 }
