ada702d9503f43dad2d81c1e7a75571a13c5e1ed
SHALINI <drshaverma242@gmail.com>
2023-06-01 21:56:29 +0530

    commit

---
+++ sizestr.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+   
+    string s1 = "nincompoop";
+    
+    // s1.insert(2, "lol");
+    for (int i = 0; i < s1.size(); i++)
+    {
+       cout<<s1[i]<<endl;
+    }
+    
+    
+    return 0;
+}
